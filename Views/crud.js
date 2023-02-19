727be954dd20e1bd58adb8e6d15ce921e75ec0a9
ProgrammerTelegram <devcollabster@gmail.com>
2023-02-19 15:47:38 -0600

    added in form validation

--- Views/crud.js
+++ Views/crud.js
@@ -5,3 +5,23 @@ let textarea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
+
+
+/**
+ * form validations
+ */
+
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
+   formValidations();
+});
+
+let formValidations = () => {
+    if(textInput.value === ""){
+        console.log("Fail form");
+        msg.innerHTML = "Task cannot be blank";
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+    }
+}
