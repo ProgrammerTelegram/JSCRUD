4c9924d73c94d9260e83e0b9261ccbe2978a1e7d
ProgrammerTelegram <devcollabster@gmail.com>
2023-02-19 18:11:53 -0600

    added refactor

--- Views/crud.js
+++ Views/crud.js
@@ -6,66 +6,19 @@ let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
-
-/**
- * form validations
- */
-
-form.addEventListener("submit", (e) => {
-   e.preventDefault();
-   formValidations();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    formValidation();
 });
 
-let formValidations = () => {
+let formValidation = () => {
     if(textInput.value === ""){
-        console.log("Fail form");
-        msg.innerHTML = "Task cannot be blank";
+       console.log('failure');
     } else {
-        console.log("success");
-        msg.innerHTML = "";
-        acceptData();
-        //modal close
-        add.setAttribute("data-bs-dismiss", "modal");
-        add.click();
-        (() => {
-            add.setAttribute("data-bs-dismiss", "");
-        })();
+        console.log('success')
     }
 }
 
-/**
-*local data storage using array
- */
-let data = [];
-let acceptData = () => {
-    data.push({
-        text: textInput.value,
-        date: dateInput.value,
-        description: textarea.value,
-
-    });
-    localStorage.setItem("data",JSON.stringify(data));
-    console.log(data);
-
-    createTasks();
-}
 
 
-/**
- *Create new task using template literal to create HTML elements and .map to push the data collected from data array
- */
-let createTasks = () => {
-    tasks.innerHTML = "";
-    data.map((x, y) => {
-        return (tasks.innerHTML += `
-        <div id=${y}>
-<span class="fw-bold">${x.text}</span>
-<span class="small text-secondary">${x.date}</span>
-<p>${x.description}</p>
-<span class="options">
-<i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-<i onclick="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
-</span>`);
-    });
-    resetForm();
-}
+
