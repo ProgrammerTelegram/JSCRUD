50885e3aa8a16224ca40f733d114ae65331d6e9c
ProgrammerTelegram <devcollabster@gmail.com>
2023-02-19 18:42:27 -0600

    add (crud) update to view

--- Views/crud.js
+++ Views/crud.js
@@ -15,10 +15,36 @@ let formValidation = () => {
     if(textInput.value === ""){
         msg.innerHTML = "Task cannot be blank";
     } else {
-        console.log('success')
+        console.log('success');
+        msg.innerHTML = "";
+        acceptData();
     }
-}
+};
 
+let data = {};
+
+let acceptData = () => {
+    data["text"] = textInput.value;
+    data["date"] = dateInput.value;
+    data["description"] = textarea.value;
+    console.log(data);
+    createTasks();
+
+};
+
+let createTasks = () => {
+tasks.innerHTML += `
+  <div>
+            <span class="fw-bold">${data.text}</span>
+            <span class="fw-bold small">${data.date}</span>
+            <p>${data.description}</p>
+            <span class="options">
+                <i class="fas fa-edit"></i>
+                <i class="fas fa-trash-alt"></i>
+            </span>
+        </div>
+`;
+};
 
 
 
