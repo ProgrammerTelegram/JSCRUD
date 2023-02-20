406b87a223e2c0410271c55ac4e73297a197625e
ProgrammerTelegram <devcollabster@gmail.com>
2023-02-19 21:04:03 -0600

    delete using local storage

--- Views/crud.js
+++ Views/crud.js
@@ -61,6 +61,10 @@ resetForm();
 
 let deleteTask = (e) => {
     e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.id,1);
+    localStorage.setItem("data", JSON.stringify(data));
+
+    console.log(data);
 };
 
 let editTask = (e) => {
